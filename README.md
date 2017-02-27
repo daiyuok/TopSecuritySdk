e57f8c26df0f98582453d90293f22cc15248012d
戴新宇 <daixinyu@shopex.cn>
2017-02-27 14:00:48 +0800

    edit readme.md

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# TopSecuritySdk
+# TopSecuritySdk 
+---
+**用途**
+
+实现Java 或者python 调用 taobao Top 加密数据解密 的SDK 封装
+
+---
+
+---
+**1.	修改配置文件**
+
+    配置文件
+
+serverUrl=https://gw.api.tbsandbox.com/router/rest
+
+appkey=                   
+    
+appSecret=
+
+randomNum=
+
+---
+
+---
+**2.	构建安装**
+
+ 1. 使用maven构建
+ mvn clean package
+
+目标生成文件在target/top-security-sdk-1.0.jar
+
+---
+
+---
+**3.	用法**
+
+ SecurityUtil securityUtil = new SecurityUtil();
+ 
+ util.isEncryptData(receiver_mobile, Constant.PHONE)#判断电话是否已加密
+ 
+ util.encryptPhone("18721883292", sessionKey) #加密电话号码
+ 
+ String receiver_mobile = "$8SPcn5YcviStk1oP01BhNg==$1ejKajhsndi0370DKbiU/w==$1$$";
+ 
+ util.decryptPhone(receiver_mobile, sessionKey)#解密电话号码
+ 
+public static final String PHONE = "phone";
+
+public static final String NICK = "nick";
+
+public static final String RECEIVER_NAME = "receiver_name";
+
+public static final String NORMAL = "normal";
+
+public static final String SIMPLE = "simple";
+
+**详细使用请查看[TestMatrixSecurityClient.java](https://github.com/daiyuok/TopSecuritySdk/blob/master/src/test/java/TestMatrixSecurityClient.java)**
+
+---
+
+
+
+
--- src/test/java/TestMatrixSecurityClient.java
+++ src/test/java/TestMatrixSecurityClient.java
@@ -9,34 +9,34 @@ public class TestMatrixSecurityClient {
 
     public static void main(String args[]) {
 
-        SecurityUtil securityUtil = new SecurityUtil();
+        SecurityUtil util = new SecurityUtil();
 
         String sessionKey = "";
 //
-//        System.out.println(securityUtil.encryptPhone("18721883292", sessionKey));
+        System.out.println(util.encryptPhone("18721883292", sessionKey));
 
         String receiver_mobile = "$8SPcn5YcviStk1oP01BhNg==$1ejKajhsndi0370DKbiU/w==$1$$";
 
-        System.out.println(securityUtil.decryptPhone(receiver_mobile, sessionKey));
+        System.out.println(util.decryptPhone(receiver_mobile, sessionKey));
 
-//        System.out.println(securityUtil.isEncryptData(receiver_mobile, Constant.PHONE));
-//
-//        String buyer_email = "~p5PQ2sX2DTcDmS5SZtvqkth2CXggVAVlPgJNF7qmbMk=~ljJyHfKsQFTNtkkcXBJ7x7V33kD1EAw3lSAoqRQdTvxYkSiRqc7ax+4TljJyHfKsQFTNtkkcJOcXlusBgqXVK0TW~1~~";
-//
-//        System.out.println(securityUtil.isEncryptData(buyer_email, Constant.SIMPLE));
-//
-//        System.out.println(securityUtil.decryptSimple(buyer_email, sessionKey));
-//
-//        String receiver_phone = "~8N5VevW6SSX0EI+GWzTYXg==~1~";
-//
-//        System.out.println(securityUtil.decryptSimple(receiver_phone, sessionKey));
+        System.out.println(util.isEncryptData(receiver_mobile, Constant.PHONE));
+
+        String buyer_email = "~p5PQ2sX2DTcDmS5SZtvqkth2CXggVAVlPgJNF7qmbMk=~ljJyHfKsQFTNtkkcXBJ7x7V33kD1EAw3lSAoqRQdTvxYkSiRqc7ax+4TljJyHfKsQFTNtkkcJOcXlusBgqXVK0TW~1~~";
+
+        System.out.println(util.isEncryptData(buyer_email, Constant.SIMPLE));
+
+        System.out.println(util.decryptSimple(buyer_email, sessionKey));
+
+        String receiver_phone = "~8N5VevW6SSX0EI+GWzTYXg==~1~";
+
+        System.out.println(util.decryptSimple(receiver_phone, sessionKey));
 
         String receiver_name = "~8BAXbKiBsoj+qjcopgIaRQ==~hRWfe1d3~1~~";
 
-        System.out.println(securityUtil.decryptReceiverName(receiver_name, sessionKey));
+        System.out.println(util.decryptReceiverName(receiver_name, sessionKey));
 
         String buyer_nick = "~k4g46TQTVWLvUrKWDSPXDQ==~dm8cMDeuTMvtJ6n7~1~~";
 
-        System.out.println(securityUtil.decryptNick(buyer_nick, sessionKey));
+        System.out.println(util.decryptNick(buyer_nick, sessionKey));
     }
 }
